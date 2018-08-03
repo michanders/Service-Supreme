edfaac669fb13b92348f0d75f91bdc0bb00b9075
michanders <michanders@msn.com>
2018-08-02 17:14:43 -0700

    updates

--- db/migrate/20180712172221_create_vehicles.rb
+++ db/migrate/20180712172221_create_vehicles.rb
@@ -1,4 +1,4 @@
-class CreateVehicles < ActiveRecord::Migration[5.2]
+class CreateVehicles < ActiveRecord::Migration[5.1]
   def change
     create_table :vehicles do |t|
       t.string :make
--- db/migrate/20180717214700_create_auto_logs.rb
+++ db/migrate/20180717214700_create_auto_logs.rb
@@ -1,9 +1,9 @@
-class CreateAutoLogs < ActiveRecord::Migration[5.2]
+class CreateAutoLogs < ActiveRecord::Migration[5.1]
   def change
     create_table :auto_logs do |t|
       t.text :entry
       t.belongs_to :vehicle
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
